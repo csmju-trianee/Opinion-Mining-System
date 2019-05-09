8aafb6794f35618742b9d48ff0138a58b76b2906
dommerer <40911678+dommerer@users.noreply.github.com>
2019-05-09 18:06:32 +0700

    change floder

---
+++ bas/test.py
@@ -0,0 +1,3 @@
+urls = "https://www.tripadvisor.com/Attraction_Review-g293917-d7133132-Reviews-Maya_Lifestyle_Shopping_Center-Chiang_Mai.html"
+id_review  = urls.split('https://www.tripadvisor.com/')[1].split('s-')
+print(id_review[0])
